4342a1511352951436619217d78fe7693970f323
Daniel Chibuogwu <danielchibuogwu17@gmail.com>
2024-05-06 20:49:38 +0100

    Shadcn is working successfully

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
-import { cn } from "@/utils/cn";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 function App() {
   return (
     <div className="w-full h-screen bg-sky-400 flex justify-center items-center">
-      <p className={cn("text-2xl font-bold")}>Just do It Daniel</p>
+      <AlertDialog>
+        <AlertDialogTrigger>Open</AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction>Continue</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </div>
   );
 }
