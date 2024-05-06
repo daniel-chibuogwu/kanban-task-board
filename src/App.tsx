8dc4b92979b7012d81df71b3e12f4ed8f2f3de23
Daniel Chibuogwu <danielchibuogwu17@gmail.com>
2024-05-06 19:14:56 +0100

    Added the cn utils function for concatenating and merging tailwind classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { cn } from "@/utils/cn";
+
 function App() {
   return (
     <div className="w-full h-screen bg-sky-400 flex justify-center items-center">
-      <p className=" text-2xl font-bold">Just do It Daniel</p>
+      <p className={cn("text-2xl font-bold")}>Just do It Daniel</p>
     </div>
   );
 }
