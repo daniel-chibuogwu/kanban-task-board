e1c19e14e2be48eaa00356a39936621ff0871443
Daniel Chibuogwu <danielchibuogwu17@gmail.com>
2024-05-30 22:37:53 +0100

    Fixed the main board layout, using transition on the margin to animate the side panel in and out and use flex-1 to make the board occupy the entire space as needed

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,30 @@
 import { useState } from 'react';
 import Board from '@/components/Board';
 import LogoDark from '@/assets/logo-dark.svg';
-import { motion, AnimatePresence } from 'framer-motion';
+import { cn } from './lib/utils';
 
 function App() {
   const [showSideNav, setShowSideNav] = useState(true);
-  const variants = {
-    open: {
-      width: 300,
-      opacity: 1,
-      transition: { type: 'spring', stiffness: 300, damping: 30 },
-    },
-    closed: {
-      width: 0,
-      opacity: 0,
-      transition: { type: 'spring', stiffness: 400, damping: 40 },
-    },
-  };
+
   return (
     <div className="relative flex min-h-screen overflow-hidden">
-      <AnimatePresence>
-        {showSideNav && (
-          <motion.nav
-            layout
-            className="relative z-50 h-screen border-r border-border bg-white pl-8 pr-6 pt-8"
-            variants={variants}
-            initial="closed"
-            animate={showSideNav ? 'open' : 'closed'}
-            exit="closed"
-          >
-            <div className="">
-              <LogoDark />
-            </div>
-          </motion.nav>
+      <nav
+        className={cn(
+          'h-screen w-[300px] border-r border-border bg-white pl-8 pr-6 pt-8 transition-[margin] duration-700 ease-in-out',
+          { 'ml-[-300px]': !showSideNav },
         )}
-      </AnimatePresence>
-      <motion.div className="grow" layout>
-        <Board showNav={showSideNav} setShowNav={setShowSideNav} />
-      </motion.div>
+      >
+        <div
+          className={cn(
+            { 'opacity-0': !showSideNav },
+            'transition-opacity duration-300 ease-in-out',
+          )}
+        >
+          <LogoDark />
+        </div>
+      </nav>
+
+      <Board showNav={showSideNav} setShowNav={setShowSideNav} />
     </div>
   );
 }
