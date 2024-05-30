dbd06d11e9a25fdb985ec9c59f27528c22cf264c
Daniel Chibuogwu <danielchibuogwu17@gmail.com>
2024-05-30 22:39:01 +0100

    Develop (#1)
    * working on dashboard layout and just saving
    
    * Fixed the main board layout, using transition on the margin to animate the side panel in and out and use flex-1 to make the board occupy the entire space as needed

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -4,9 +4,9 @@ import { type NavBtnProps } from '@/lib/types';
 
 export default function Board({ showNav, setShowNav }: NavBtnProps) {
   return (
-    <main className="relative z-20 h-svh overflow-auto bg-background">
+    <main className="relative z-20 flex h-svh flex-1 flex-col overflow-auto bg-background">
       <BoardHeader showNav={showNav} setShowNav={setShowNav} />
-      <div className=" grid h-full place-content-center">
+      <div className="grid flex-1 place-content-center">
         <div className="flex flex-col items-center justify-center">
           <p className="text-center text-lg font-bold text-foreground">
             This board is empty. Create a new column to get started.
--- src/components/BoardHeader.tsx
+++ src/components/BoardHeader.tsx
@@ -2,16 +2,21 @@ import LogoDark from '@/assets/logo-dark.svg';
 import KebabMenuIcon from '@/assets/icon-vertical-ellipsis.svg';
 import { Button } from '@/components/ui/Button';
 import { type NavBtnProps } from '@/lib/types';
+import { cn } from '@/lib/utils';
 
 export default function BoardHeader({ showNav, setShowNav }: NavBtnProps) {
   return (
-    <header className="sticky inset-x-0 top-0 z-10 flex h-24 items-center border-b border-b-border bg-white pl-6 pr-8">
-      {!showNav && (
-        <div className="mr-8 flex h-full items-center border-r border-border pr-8">
-          <LogoDark />
-        </div>
-      )}
-      <h1 className="text-card-header text-xl font-bold">Platform Launch</h1>
+    <header className="sticky inset-x-0 top-0 flex h-24 shrink-0 items-center border-b border-b-border bg-white pl-6 pr-8">
+      <div
+        className={cn(
+          'mr-8 flex h-full items-center border-r border-border pr-8 transition-opacity duration-1000',
+          { hidden: showNav },
+        )}
+      >
+        <LogoDark />
+      </div>
+
+      <h1 className="text-xl font-bold text-card-header">Platform Launch</h1>
       <Button
         className="ml-auto mr-6"
         disabled={true}
